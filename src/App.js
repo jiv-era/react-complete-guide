53513c65ed32fae19eba0600911b8f4acc9afad1
Jordan R <jaygerzero@gmail.com>
2020-03-04 18:04:29 -0500

    applying onClick handler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Jordan', age: 29 },
+      { name: 'Austin', age: 25 },
+      { name: 'Josh', age: 28 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('was clicked')
+  }
+
   render() {
     return (
       <div className="App">
-       <h1>Hi, I'm a react app</h1>
-       <p>This is really working!</p>
+        <h1>Hi, I'm a react app</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} > My Hobbies: Coding!</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}  />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null,  'Does this work now?'));
